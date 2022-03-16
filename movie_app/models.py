67305255d1d8a76f4096935cec873d1280155125
Alimsaid <alimsaid242@gmail.com>
2022-03-16 21:42:45 +0600

    alim

--- movie_app/models.py
+++ movie_app/models.py
@@ -3,15 +3,47 @@ from django.db import models
 
 class Director(models.Model):
     name = models.CharField(max_length=100)
+    # movie = models.ForeignKey(Movie, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     duration = models.PositiveIntegerField()
-    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def filtered_reviews(self):
+        return self.reviews.filter(stars__gte=4)
+
+    @property
+    def reviews_count(self):
+        return self.filtered_reviews.count()
+
+    @property
+    def rating(self):
+        return 'f{CHOICES}'.average(self.rating)
+
+
+CHOICES = (
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
+)
 
 
 class Review(models.Model):
     text = models.TextField(max_length=300)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.SET_NULL, null=True, related_name='reviews')
+    stars = models.IntegerField(default=1, choices=CHOICES)
+
+    def __str__(self):
+        return self.text
--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -4,19 +4,34 @@ from movie_app.models import Movie
 from movie_app.models import Review
 
 
-class DirectorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Director
-        fields = '__all__'
-
-
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = '__all__'
-
-
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
+
+
+class DirectorSerializer(serializers.ModelSerializer):
+    movie = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Director
+        fields = 'id name movie'.split()
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    director = DirectorSerializer()
+    # reviews = ReviewSerializer(many=True)
+    reviews = serializers.SerializerMethodField()
+    reviews_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+    def get_reviews(self, movie):
+        # filtered_reviews = Review.objects.filter(movie=movie, stars__gte=4)
+        filtered_reviews = movie.reviews.filter(stars__gte=4)
+        return ReviewSerializer(filtered_reviews, many=True).data
+
+    def get_reviews_count(self, movie):
+        return movie.reviews.filter(stars__gte=4).count()
